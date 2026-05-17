af0c2be03eb4fead78273fb60d2fae1d8df420aa
edumarcal87 <edumarcal87@gmail.com>
2026-05-17 18:35:57 -0300

    adiciona deploy automatico via github actions

--- tina/__generated__/types.d.ts
+++ tina/__generated__/types.d.ts
@@ -1877,7 +1877,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/1.6/content/f70b7efa-4a9f-423d-a4b4-a69764b5915e/github/main",
         queries,
       })
     )
--- tina/config.js
+++ tina/config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 
   build: {
     outputFolder: "admin",
-    publicFolder: "public",
+    publicFolder: "dist",
   },
 
   media: {
