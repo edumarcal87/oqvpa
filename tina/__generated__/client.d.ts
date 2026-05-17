a00cd001b8313020e47cad891725b68c023d4900
edumarcal87 <edumarcal87@gmail.com>
2026-05-17 19:55:05 -0300

    gera admin no ci via tinacms build

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: 'C:/web_app/oqvpa/tina/__generated__/.cache/1779056336242', url: 'https://content.tinajs.io/1.6/content/f70b7efa-4a9f-423d-a4b4-a69764b5915e/github/main', token: '84c81f186c7da56182c6b027e7f5df495c32093f', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: '84c81f186c7da56182c6b027e7f5df495c32093f', queries,  });
 export default client;
   
